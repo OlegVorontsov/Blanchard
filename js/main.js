2260f8d3013b69b28a236780d182988b3244f7f7
Олег Воронцов <oleg-voroncov@mail.ru>
2024-04-03 15:17:50 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const gallerySlider = new Splide('#gallerySlider', {
+  perPage: 3,
+  gap: '50px',
+  pagination: false,
+  breakpoints: {
+    500: {
+      perPage: 1,
+      arrows: false,
+      pagination: true
+    }
+  }
+})
+
+const eventsSlider = new Splide('#eventsSlider', {
+  perPage: 3,
+  gap: '50px',
+  pagination: false,
+  breakpoints: {
+    768: {
+      perPage: 1,
+      arrows: false,
+      pagination: true
+    }
+  }
+})
+
+gallerySlider.mount()
+eventsSlider.mount()
+
+
+
