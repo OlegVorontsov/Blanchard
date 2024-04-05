86c02479dd008ecc0ea425bdedab1e040daf8f00
Vorontsov Home PC <oleg-voroncov@mail.ru>
2024-04-05 21:07:49 +0300

    map added

--- js/script.js
+++ js/script.js
@@ -13,5 +13,23 @@ window.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+function init() {
+  let map = new ymaps.Map('map', {
+    center: [55.75846806898367,37.60108849999989],
+    zoom: 17
+  })
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+
+
+
+ymaps.ready(init);
 
 
